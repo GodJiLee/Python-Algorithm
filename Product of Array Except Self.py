f711606031fd70c1655882932044aa79546e55ff
Jiwon Lee <53148677+GodJiLee@users.noreply.github.com>
2021-02-14 21:39:02 +0900

    #238 Product of Array Except Self

---
+++ Product of Array Except Self.py
@@ -0,0 +1,33 @@
+nums = [1, 2, 3, 4]
+
+# Two-Pointer
+products = [1 for x in range(len(nums))]
+for i in range(len(nums)):
+    left, right = i - 1, i + 1
+
+    while left >= 0:
+        products[i] *= nums[left]
+        if left > 0:
+            left -= 1
+        else:
+            break
+
+    while right <= len(nums) - 1:
+        products[i] *= nums[right]
+        if right < len(nums) - 1:
+            right += 1
+        else:
+            break
+
+# 1.
+out = []
+p = 1
+# left side product
+for i in range(0, len(nums)):
+    out.append(p)
+    p = p * nums[i]
+p = 1
+# right side products
+for i in range(len(nums) - 1, 0 - 1, -1):
+    out[i] = out[i] * p
+    p = p * nums[i]
